994743852062fd487464989a55965de2de7b109e
migsilva89 <migdrum@gmail.com>
2022-03-09 14:09:59 +0100

    done!]

--- Portfolio Page/index.js
+++ Portfolio Page/index.js
@@ -3,35 +3,33 @@ AOS.init();
 
 window.onscroll = function() {scrollFunction()};
 
-// function scrollFunction() {
-//   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-//     document.getElementById("navbar").style.visibility = "visible";
-//     document.getElementById("navbar").classList.add("pb-5","bg-black");
-//     document.getElementById("ul1").classList.add("pt-5");
-//   } else if (document.body.scrollTop < 10 || document.documentElement.scrollTop < 10) {
-//     document.getElementById("navbar").style.visibility = "visible";
-//   } else  {
-//     document.getElementById("navbar").style.visibility = "hidden";
-//   }
-// }
-
-var prevScrollpos = window.pageYOffset;
-
-window.onscroll = function() {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.visibility = "visible";
-    } else {
-        document.getElementById("navbar").style.visibility = "hidden";
-        document.getElementById("navbar").classList.add("pb-5","bg-black");
-        document.getElementById("ul1").classList.add("pt-5");
-    }
-    prevScrollpos = currentScrollPos;
-
-    if (document.documentElement.scrollTop < 400) {
-            document.getElementById("navbar").classList.remove("pt-8", "bg-black");
-    } 
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementById("navbar").style.visibility = "visible";
+    document.getElementById("navbar").classList.add("pb-5","bg-black");
+    document.getElementById("ul1").classList.add("pt-5");
+  } else {
+    document.getElementById("navbar").style.visibility = "visible";
+  }
 }
+
+// var prevScrollpos = window.pageYOffset;
+
+// window.onscroll = function() {
+//     var currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("navbar").style.visibility = "visible";
+//     } else {
+//         document.getElementById("navbar").style.visibility = "hidden";
+//         document.getElementById("navbar").classList.add("pb-5","bg-black");
+//         document.getElementById("ul1").classList.add("pt-5");
+//     }
+//     prevScrollpos = currentScrollPos;
+
+//     if (document.documentElement.scrollTop < 400) {
+//             document.getElementById("navbar").classList.remove("pt-8", "bg-black");
+//     } 
+// }
 
     // window.onscroll = function() {scrollFunction()};
     
